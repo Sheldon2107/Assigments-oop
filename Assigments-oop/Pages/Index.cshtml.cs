3a63de43ee343b8435638ae7ff9e403261bb0122
Nadzira <nadzira_22010431@utp.edu.my>
2024-10-19 20:43:36 +0800

    Update Index.cshtml.cs

--- Assigments-oop/Pages/Index.cshtml.cs
+++ Assigments-oop/Pages/Index.cshtml.cs
@@ -5,12 +5,17 @@ namespace ASSIGMENTS.Pages
     public class IndexModel : PageModel
     {
         // This property will hold a simple message to be displayed on the page
-        public string WelcomeMessage { get; private set; }
-
+        public string WelcomeMessage="";//{ get(); private set;}
         public void OnGet()
         {
             // Set a welcome message when the page is accessed
             WelcomeMessage = "Welcome to the Project Management Dashboard!";
+        }
+        public string get(){
+            return WelcomeMessage;
+        }
+        public void set( string n){
+            WelcomeMessage=n;
         }
     }
 }
