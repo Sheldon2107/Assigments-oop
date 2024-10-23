afa41f3dd55ba475711005edcb9ac605477ab7a8
Nadzira <nadzira_22010431@utp.edu.my>
2024-10-23 11:11:09 +0800

    add process class to projectpackage
    adding process class and saving class to project package

--- Assigments-oop/obj/Debug/net8.0/Assigments-oop.AssemblyInfo.cs
+++ Assigments-oop/obj/Debug/net8.0/Assigments-oop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assigments-oop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a63de43ee343b8435638ae7ff9e403261bb0122")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5419294dde82ae0aac5a90ad00199e7e788c2537")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assigments-oop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assigments-oop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ProjectPackage/process.cs
+++ ProjectPackage/process.cs
@@ -5,4 +5,24 @@ namespace ProjectPackage;
 public class process
 {
 
+    public saving saving =  new saving();
+    private const string FIREBASE_PROJID = "kampungManage";
+    private FirestoreDb db;
+    private FirestoreDb db;
+
+    public Business(){
+        TransList = new TransactionList();
+    }
+    public void initFirestore() // all initialization of firebase
+    {
+        FirebaseApp.Create();
+        db = FirestoreDb.Create(FIREBASE_PROJID);
+        Console.WriteLine("Created Cloud FireStore client with project ID: {0}", FIREBASE_PROJID);
+    }
+
+    
+
+
+
+
 }
